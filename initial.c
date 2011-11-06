33d75c16768bae53ebe80877d112d2b5997e0726
Brendan Le Foll <brendan@fridu.net>
2011-11-06 23:38:34 +0000

    remove old file and add license

--- initial.c
+++ initial.c
@@ -1,7 +1,8 @@
-/*  
- * OCCCL - occam -> C/opencl interopability
+/* Simple library meant to be called from occam that serves as proof 
+ * of concept for occam -> C/opencl interopability
  *
- * Copyright 2011 - Brendan Le Foll - bl73@kent.ac.uk
+ * Copyright 2011 - Brendan Le Foll - brendan@fridu.net
+ * Copyright 2011 - University of Kent
  */
 
 #include "initial.h"
--- rot13.c
+++ rot13.c
@@ -1,7 +1,8 @@
 /* Simple library meant to be called from occam that serves as proof 
  * of concept for occam -> C/opencl interopability
  *
- * Copyright 2011 - Brendan Le Foll - bl73@kent.ac.uk
+ * Copyright 2011 - Brendan Le Foll - brendan@fridu.net
+ * Copyright 2011 - University of Kent
  */
 
 #include "rot13.h"
